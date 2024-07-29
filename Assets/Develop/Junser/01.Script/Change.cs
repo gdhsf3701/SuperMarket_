2e5d1f57f80d9e361f3f0d6e880375f0cba77a53
박준서 <pppg12jintpt@gmail.com>
2024-07-29 13:19:04 +0900

    변★신

--- Assets/Develop/Junser/01.Script/Change.cs
+++ Assets/Develop/Junser/01.Script/Change.cs
@@ -1,13 +1,19 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class Change : MonoBehaviour
 {
-    private void Changed()
+    
+    private void ChangeToCar()
     {
-        
+        PlayerMovement movement = GetComponent<PlayerMovement>();
+        movement.enabled = false;
+        CarMove car = GetComponent<CarMove>();
+        car.enabled = true;
     }
 }
--- Assets/Develop/Junser/01.Script/ItemScript.cs
+++ Assets/Develop/Junser/01.Script/ItemScript.cs
@@ -8,14 +8,16 @@ public class ItemScript : MonoBehaviour
     [SerializeField]
     private int expAmount;
     private Change playerLevel;
+
+    private MeshFilter meshFilter;
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             gameObject.SetActive(false);
             playerLevel = other.GetComponent<Change>();
-            playerLevel.TakeXP(expAmount);
-            other.AddComponent<>();
+            meshFilter = other.GetComponent<MeshFilter>();
+            meshFilter.mesh = gameObject.GetComponent<MeshFilter>().mesh;
         }
     }
 }
